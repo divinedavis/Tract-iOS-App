c0b38bb84489044ed0d868c053f22a71566b5bbc
divinedavis <divinejdavis@gmail.com>
2015-09-19 18:29:45 -0400

    First Commit

---
+++ Tract/Tract/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tract
+//
+//  Created by Divine Davis on 9/19/15.
+//  Copyright © 2015 Juniper Street. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
