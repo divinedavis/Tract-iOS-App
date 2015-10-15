55d0be157750f0155f8cf4048b827b84ed5872ea
divinedavis <divinejdavis@gmail.com>
2015-10-15 18:39:50 -0400

    Implementing a RatingControl view so different mechanics can be rated based on past customers.

---
+++ Tract/Tract/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  Tract
+//
+//  Created by Divine Davis on 10/15/15.
+//  Copyright © 2015 Juniper Street. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
