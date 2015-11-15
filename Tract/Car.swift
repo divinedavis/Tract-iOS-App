52ae526640ff018c99e5063a9018f685a17b36ef
divinedavis <divinejdavis@gmail.com>
2015-11-14 23:57:46 -0500

    BUGS

--- Tract/Car.swift
+++ Tract/Car.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-typealias ServiceResponse = (JSON, NSError?) -> Void
 
 
 class Car: NSObject {
@@ -19,28 +18,6 @@ class Car: NSObject {
     var color : String = ""
     var body : String = ""
     
-    static let sharedInstance = Car()
-    
-    let baseURL = "http://api.randomuser.me/"
-    
-    func getRandomUser(onCompletion: (JSON) -> Void) {
-        let route = baseURL
-        makeHTTPGetRequest(route, onCompletion: { json, err in
-            onCompletion(json as JSON)
-        })
-    }
-    
-    func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
-        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
-        
-        let session = NSURLSession.sharedSession()
-        
-        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            let json:JSON = JSON(data: data)
-            onCompletion(json, error)
-        })
-        task.resume()
-    }
     
 }
 
