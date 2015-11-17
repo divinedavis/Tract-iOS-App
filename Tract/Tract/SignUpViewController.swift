88f21f0fb67b9316eedd1f86da77a210a44e31f7
divinedavis <divinejdavis@gmail.com>
2015-11-16 21:22:42 -0500

    Done with sign in / sign up view - Reducing error with signing up

--- Tract/Tract/SignUpViewController.swift
+++ Tract/Tract/SignUpViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var detailLabel: UILabel!
+  
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
-
+    
     @IBOutlet weak var password2TextField: UITextField!
     
-    @IBAction func signInSignUpButton(sender: AnyObject) {
+    @IBOutlet weak var forgotPasswordLabel: UILabel!
+   
+    @IBOutlet weak var signInSignUpButton: UIButton!
+    
+    var signupActive : Bool = true
+
+    @IBOutlet weak var toggleSignUp: UIButton!
+    
+    @IBAction func forgotPasswordButton(sender: AnyObject) {
         
-        self.view.endEditing(true)
+        passwordTextField.hidden = true
+        password2TextField.hidden = true
+        signInSignUpButton.setTitle("Send new password", forState: UIControlState.Normal)
+        toggleSignUp.hidden = true
+    }
+    @IBAction func signInSignUpButton(sender: AnyObject) {
         
         if password2TextField.text != passwordTextField.text {
             
@@ -29,40 +44,68 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
             self.presentViewController(alert, animated: true, completion: nil)
         }
         
-        if ((emailTextField.text?.isEmpty) != nil) {
+        if emailTextField.text == "" {
             
             var alert = UIAlertController(title: "Email", message: "You did not provide a correct email", preferredStyle: UIAlertControllerStyle.Alert)
             
             alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
             
             self.presentViewController(alert, animated: true, completion: nil)
-            
+    }
+        func textFieldShouldReturn(userText: UITextField!) -> Bool {
+            userText.resignFirstResponder()
+            return true;
         }
+    }
+    
+
+    @IBAction func toggleSignInSignUpButton(sender: AnyObject) {
         
+        if signupActive == true {
+            
+            signupActive = false
+            
+            detailLabel.text = "Sign up below"
+            signInSignUpButton.setTitle("Log in", forState: UIControlState.Normal)
+            
+            forgotPasswordLabel.text = "Already a member?"
+            signInSignUpButton.setTitle("Sign up", forState: UIControlState.Normal)
+            
+            toggleSignUp.setTitle("Log in", forState: UIControlState.Normal)
+            
+            password2TextField.hidden = false
+            
+            print("Sign up view")
+            
+        } else {
+            
+            signupActive = true
+            
+            detailLabel.text = "Sign in below"
+            signInSignUpButton.setTitle("Sign up", forState: UIControlState.Normal)
+            
+            forgotPasswordLabel.text = "Not a member?"
+            
+            toggleSignUp.setTitle("Sign up", forState: UIControlState.Normal)
+            signInSignUpButton.setTitle("Sign me In!", forState: UIControlState.Normal)
+            
+            password2TextField.hidden = true
+            
+            print("Sign in view")
+        }
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         
         self.view.endEditing(true)
         
         super.viewDidLoad()
         
         self.emailTextField.delegate = self
-        
-        func textFieldShouldReturn(userText: UITextField!) -> Bool {
-            userText.resignFirstResponder()
-            return true;
-        }
-        
-        
-        
-        view.resignFirstResponder()
-        
-       
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
@@ -77,5 +120,7 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    
+   
 
 }
